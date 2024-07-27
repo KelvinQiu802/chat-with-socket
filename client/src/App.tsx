b2ce2a513fcdaf25e84e3131f1e1a9e0670d71ca
KelvinQiu <kelvinqiu802@outlook.com>
2024-07-27 15:38:20 +0800

    feat: chatroom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,59 @@
+import { useState } from 'react';
+import Chat from './components/Chat';
 import { Button } from './components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './components/ui/card';
+import { Input } from './components/ui/input';
 
 function App() {
+  const [userName, setUserName] = useState('');
+  const [users, setUsers] = useState<string[]>([]);
+
+  const handleCreate = () => {
+    setUsers((prev) => [...prev, userName]);
+    setUserName('');
+  };
+
   return (
     <div>
-      <Button>Hello</Button>
+      <Card className='m-10'>
+        <CardHeader>
+          <CardTitle>Chat Panal</CardTitle>
+          <CardDescription>WebSocket Chat Playground</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className='flex w-full max-w-sm items-center space-x-2'>
+            <Input
+              type='text'
+              placeholder='User Name'
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              autoFocus
+            />
+            <Button
+              type='submit'
+              onClick={handleCreate}
+              disabled={userName.replace(' ', '') === ''}
+            >
+              Create Chat
+            </Button>
+          </div>
+          <div className='flex flex-wrap gap-7 mt-7'>
+            {users.map((u) => (
+              <Chat userName={u} key={u} />
+            ))}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <p className='text-gray-500 text-sm'>@KelvinQiu802</p>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,45 +1,110 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createMessage } from '../utils/message';
+import { Button } from './ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import { Input } from './ui/input';
 
-let ws: WebSocket | null = null;
+interface Props {
+  userName: string;
+}
 
-function Chat() {
+function Chat({ userName }: Props) {
   const [connected, setConnected] = useState(false);
-  const [name, setName] = useState('');
   const [msg, setMsg] = useState('');
+  const [history, setHistory] = useState<string[]>([]);
+  const [ws, setWs] = useState<WebSocket | null>(null);
 
   const handleConnect = () => {
     if (ws) return;
-    ws = new WebSocket('ws://localhost:8080');
-
-    ws.onopen = () => {
-      console.log('Connected');
-      setConnected(true);
-      if (ws) {
-        ws.send(JSON.stringify(createMessage(name, 'JOIN')));
-      }
-    };
-
-    ws.onmessage = (e) => {
-      const msg = e.data;
-      console.log(msg);
-    };
+    setWs(new WebSocket('ws://localhost:8080'));
   };
 
   const handleSendMsg = () => {
     if (!ws || !connected) return;
     ws.send(JSON.stringify(createMessage(msg)));
+    setMsg('');
+    setHistory((prev) => [...prev, `You: ${msg}`]);
   };
 
   const handleLeave = () => {
     if (!ws || !connected) return;
-    ws.send(JSON.stringify(createMessage(name, 'LEAVE')));
+    ws.send(JSON.stringify(createMessage(userName, 'LEAVE')));
     ws.close();
-    ws = null;
+    setWs(null);
     setConnected(false);
+    setHistory((prev) => [...prev, 'You left the chat']);
   };
 
-  return <div>Chat</div>;
+  useEffect(() => {
+    handleConnect();
+  }, []);
+
+  useEffect(() => {
+    if (!ws) return;
+    ws.onopen = () => {
+      console.log('Connected');
+      setConnected(true);
+      if (ws) {
+        ws.send(JSON.stringify(createMessage(userName, 'JOIN')));
+        setHistory((prev) => [...prev, 'You joined the chat']);
+      }
+    };
+
+    ws.onmessage = (e) => {
+      const msg = e.data;
+      setHistory((prev) => [...prev, msg]);
+    };
+  }, [ws]);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{userName}</CardTitle>
+        <CardDescription>{`Status: ${
+          connected ? 'online' : 'offline'
+        }`}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {history.map((h) => (
+          <p key={h}>{h}</p>
+        ))}
+      </CardContent>
+      <CardFooter>
+        <div className='flex w-full max-w-sm items-center space-x-2'>
+          <Input
+            type='text'
+            placeholder='Message'
+            className='w-60'
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+            autoFocus
+          />
+          <Button
+            type='submit'
+            disabled={!connected || !ws || !msg}
+            onClick={handleSendMsg}
+          >
+            Send
+          </Button>
+          <Button
+            type='submit'
+            variant='destructive'
+            disabled={!connected}
+            onClick={handleLeave}
+          >
+            Leave
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default Chat;
